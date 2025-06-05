4c1c71653b849f078188d63dddbc21fafbf60fd6
MiguelAngelBusto <chetobustos96@gmail.com>
2025-06-05 09:48:28 -0300

    Modificacion estetica

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ function toggleSidebar() {
 
   function mostrarContenido(opcion) {
     const contenido = {
-      docente: "<h3>Docente a Cargo</h3><p>Mg. Ing. Hugo O. Ortega</p>",
-      jtp: "<h3>Jefe de TP</h3><p>Ing. Luis Ortiz</p>",
+      docente: "<h3>Docente a Cargo</h3><p>Mg. Ing. Hugo O. Ortega</p><h3>Jefe de TP</h3><p>Ing. Luis Ortiz</p>",
       requisitos: `<h3>Requisitos</h3>
         <p>Algoritmos y Estructura de Datos</p>
         <p>Arquitectura y Organización de Computadoras</p>`,
-      teoria: `<h3>Días de Teoría</h3>
+      horarios: `<h3>Días de Teoría</h3>
         <p>Lunes 17:00 a 18:30</p>
-        <p>Miércoles 18:30 a 20:00</p>`,
-      practica: `<h3>Días de Práctica</h3>
+        <p>Miércoles 18:30 a 20:00</p>
+        <h3>Días de Práctica</h3>
         <p>Martes 19:00 a 20:30</p>
         <p>Jueves 19:00 a 20:30</p>`,
       TeoCompleta:
       `<h3>Capitulo I - Introducción a las Comunicaciones: <a href="PDF/Comunicaciones/clase 1_comun1_2025.pdf" target="_blank">Ver</a></h3>
-      <h3>Capítulo 2 - Arquitectura de Protocolos: <a href="PDF/Comunicaciones/clase 2_comun1_2025.pdf" target="_blank">Ver</a></h3>`
+      <h3>Capítulo 2 - Arquitectura de Protocolos: <a href="PDF/Comunicaciones/clase 2_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 3 - Conceptos de Señales: <a href="PDF/Comunicaciones/clase 3_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 4 - Transmisión de Datos <a href="PDF/Comunicaciones/clase 4_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 5 - Medios de transmisión: <a href="PDF/Comunicaciones/clase 5_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 6 - Codificación y Modulación de Señales: <a href="PDF/Comunicaciones/clase 6_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 7 - Redes LAN: <a href="PDF/Comunicaciones/clase 7_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 8 - Protocolo Ethernet: <a href="PDF/Comunicaciones/clase 8_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 9 - LAN Inalámbrica: <a href="PDF/Comunicaciones/clase 9_comun1_2025.pdf" target="_blank">Ver</a></h3>
+      <h3>Capítulo 10 - Normas de cableado estructurado: <a href="PDF/Comunicaciones/clase 10_comun1_2025.pdf" target="_blank">Ver</a></h3>`
     };
 
     document.getElementById("contenidoDinamico").innerHTML = contenido[opcion];
